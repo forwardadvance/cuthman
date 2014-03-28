3df9d218563bc283c1786369991efe6ec3892cbc
Nicholas Johnson <nicholas@forwardadvance.com>
2014-03-28 23:42:18 +0000

    Fixes map widget

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,27 +1,29 @@
-// (function() {
-//   $.fn.maps = function() {
-    //   var mapOptions = {
-    //     center: new google.maps.LatLng(-34.397, 150.644),
-    //     zoom: 8
-    //   };
-    //   var map = new google.maps.Map(document.getElementById("map-canvas"),
-    //       mapOptions);
-    // }
-    // google.maps.event.addDomListener(window, 'load', initialize);
+/*jslint browser: true*/
+/*global google */
 
-// })();
+$(window).ready(function () {
+  $('.map').each(function (i, el) {
+    var
+      $el = $(el),
+      lat = parseFloat($el.attr('data-lat'), 10),
+      lng = parseFloat($el.attr('data-lng'), 10),
+      zoom = parseInt($el.attr('data-zoom'), 10),
+      latLng = new google.maps.LatLng(lat, lng),
+      mapOptions = {
+        center: latLng,
+        zoom: zoom
+      },
+      map = new google.maps.Map(el, mapOptions),
+      marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: "St Cuthman's"
+      }),
+      infowindow = new google.maps.InfoWindow({
+        content: '<a href="https://www.google.co.uk/maps/place/St+Cuthmans+C+Of+E+Church/@50.82824,-0.105356,17z/data=!3m1!4b1!4m2!3m1!1s0x487585e8de2d04e5:0xf86de2535b64f4d2">View in Google Maps</a>'
+      });
+    infowindow.open(map, marker);
 
-$(window).ready(function() {
-  $('.map').each(function(i, el) {
-    var map = $(el),
-    lat = map.attr('data-lat'),
-    lng = map.attr('data-lng'),
-    zoom = parseInt(map.attr('data-zoom'));
-
-    var mapOptions = {
-      center: new google.maps.LatLng(lat, lng),
-      zoom: zoom
-    };
-    var map = new google.maps.Map(el, mapOptions);
-  })
+  });
 });
