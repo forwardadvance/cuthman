fc4b6f77ed0ce6ebdb1d53c5bd2d41ec81836374
Nicholas Johnson <nicholas@forwardadvance.com>
2014-03-28 22:06:22 +0000

    Use the S3 domain URL for Ireland S3 location

--- app/models/concerns/has_image.rb
+++ app/models/concerns/has_image.rb
@@ -22,7 +22,9 @@ module HasImage
         :s3_credentials => {
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-        }
+        },
+        :url =>':s3_domain_url',
+        :path => '/:class/:attachment/:id_partition/:style/:filename'
       }
     end
   end
