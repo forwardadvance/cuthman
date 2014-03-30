461774ba84031c2035a07bac1346c64a091013fa
Nicholas Johnson <nicholas@forwardadvance.com>
2014-03-30 11:24:53 +0100

    Changed Logo

--- app/helpers/slot_helper.rb
+++ app/helpers/slot_helper.rb
@@ -3,15 +3,17 @@ module SlotHelper
     limit = args[:limit] || 1
     output = []
     widgets = @page.widgets.where(slot: slot_name).order(:sort)
-    output << "<div class='slot #{slot_name} #{style}' data-limit='#{limit}' data-count='#{widgets.count}'>"
-    widgets.each do |widget|
-      output << widget(widget, style)
+    unless widgets.blank? || admin?
+      output << "<div class='slot #{slot_name} #{style}' data-limit='#{limit}' data-count='#{widgets.count}'>"
+      widgets.each do |widget|
+        output << widget(widget, style)
+      end
+      output << render(partial: 'layouts/admin/widget_selection')
+      if (limit >  widgets.count) && admin?
+        output << render(partial: 'layouts/admin/add_widget_button', locals: {slot_name: slot_name} )
+      end
+      output << "</div>"
     end
-    output << render(partial: 'layouts/admin/widget_selection')
-    if (limit >  widgets.count) && admin?
-      output << render(partial: 'layouts/admin/add_widget_button', locals: {slot_name: slot_name} )
-    end
-    output << "</div>"
     return output.join.html_safe
   end
 end
